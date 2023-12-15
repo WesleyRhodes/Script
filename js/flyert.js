f71d08d931471d97d4e1cabf8511796dd25c5a68
Wesley Rhodes <148964633+WesleyRhodes@users.noreply.github.com>
2023-12-15 14:15:34 +0000

    1

---
+++ js/flyert.js
@@ -0,0 +1,10 @@
+// 2023-12-15 22点08分
+let url = $request.url;
+let body = $response.body;
+console.log('dy123_let url_in'+url);
+console.log('Flyert=========================START================================================');
+
+if (url.includes("timeline_tab_name")) {
+    let obj = JSON.parse(body);
+    obj.Variables.data.data=[]; 
+    
