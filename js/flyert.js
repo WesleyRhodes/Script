a60d2d7750529d3c2d439fa5b7f1a78a72a5cadb
Wesley Rhodes <148964633+WesleyRhodes@users.noreply.github.com>
2023-12-15 14:33:09 +0000

    2

--- js/flyert.js
+++ js/flyert.js
@@ -7,8 +7,9 @@ console.log('Flyert=========================START===============================
 if (url.includes("timeline_tab_name")) {
     let obj = JSON.parse(body);
     obj.Variables.data.data=[]; 
-
-
-
-    
+    body = JSON.stringify(obj);
+    console.log('Flyert替换body成功'+JSON.stringify(obj));
+    $done({ body })
 }
+
+console.log('Flyert===============================END==========================================');
